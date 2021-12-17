0151c78c6df18381f81b2445fef2b7d55545cc68
philosanhentai <61416026+philosanhentai@users.noreply.github.com>
2021-12-17 18:10:42 +0200

    Create the_first_ever.cs

---
+++ the_first_ever.cs
@@ -0,0 +1,13 @@
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("hello user");
+            string username;
+           username = Console.ReadLine();
+            Console.WriteLine("waht a nice name "+username);
+        }
+    }
+}
